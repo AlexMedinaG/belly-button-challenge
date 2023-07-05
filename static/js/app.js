e97a88006533a0f2f01d45d397db39c7fd44db43
AlexMedinaG <medinagomez.alejandro@gmail.com>
2023-07-04 22:10:37 -0600

    Update app.js

--- static/js/app.js
+++ static/js/app.js
@@ -1,20 +1,96 @@
-//just checking for js connection in html
-console.log("app.js connected")
+//checking the file is loaded correctly
+console.log("app.js loaded");
 
-
-//Complete the following steps:
-
-//1. Use the D3 library to read in samples.json from the URL https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json.
-
+//using the D3 library to read the sample file
 d3.json("../../samples.json").then(function(data) {
-    console.log(data);
+  //accessing the required data
+  const samples = data.samples;
+  const metadata = data.metadata;
+
+  //update data based on selection of test subject
+  function updateChart(selectedIndividual) {
+    //filter the data for the selection
+    const selectedSample = samples.find(sample => sample.id === selectedIndividual);
+
+    //extraction of the topo OTUs
+    const top10OTUs = selectedSample.otu_ids.slice(0, 10);
+    const top10Values = selectedSample.sample_values.slice(0, 10);
+    const top10Labels = selectedSample.otu_labels.slice(0, 10);
+
+    //horizontal bar chart
+    const barTrace = {
+      x: top10Values.reverse(),
+      y: top10OTUs.map(otu => `OTU ${otu}`).reverse(),
+      text: top10Labels.reverse(),
+      type: "bar",
+      orientation: "h"
+    };
+    const barData = [barTrace];
+    const barLayout = {
+      title: "Top 10 OTUs",
+      yaxis: {
+        automargin: true
+      }
+    };
+    Plotly.newPlot("bar", barData, barLayout);
+
+    //bubble chart
+    const bubbleTrace = {
+      x: selectedSample.otu_ids,
+      y: selectedSample.sample_values,
+      text: selectedSample.otu_labels,
+      mode: "markers",
+      marker: {
+        size: selectedSample.sample_values,
+        color: selectedSample.otu_ids,
+      }
+    };
+    const bubbleData = [bubbleTrace];
+    const bubbleLayout = {
+      xaxis: {
+        title: "OTU ID"
+      }
+    };
+    Plotly.newPlot("bubble", bubbleData, bubbleLayout);
+
+    //display demographic info
+    displayMetadata(selectedIndividual);
+  }
+  
+  //get the metadata for the selected subject
+  function displayMetadata(selectedIndividual) {
+    const selectedMetadata = metadata.find(meta => meta.id === parseInt(selectedIndividual));
+    const metadataElement = d3.select("#sample-metadata");
+
+    //clear metadata for wehn selection changes
+    metadataElement.html("");
+
+    //appending keys and their values as paragraphs
+    Object.entries(selectedMetadata).forEach(([key, value]) => {
+      metadataElement.append("p")
+        .text(`${key}: ${value}`);
+    });
+  }
+
+  //dropdown menu
+  function optionChanged(value) {
+    updateChart(value);
+  }
+
+  //set so it starts with the first individual
+  const initialIndividual = samples[0].id;
+  updateChart(initialIndividual);
+
+  //populate dropdown menu
+  const dropdown = d3.select("#selDataset");
+  dropdown.selectAll("option")
+    .data(samples.map(sample => sample.id))
+    .enter()
+    .append("option")
+    .text(d => d)
+    .attr("value", d => d);
+  dropdown.on("change", function() {
+    const selectedValue = d3.select(this).property("value");
+    optionChanged(selectedValue);
   });
-
-//2. Create a horizontal bar chart with a dropdown menu to display the top 10 OTUs found in that individual.
-
-
-//Use sample_values as the values for the bar chart.
-
-//Use otu_ids as the labels for the bar chart.
-
-//Use otu_labels as the hovertext for the chart.
+});
